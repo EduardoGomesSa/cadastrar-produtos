ab323f60942982ed78107144183455bcaae5675e
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-20 19:49:46 -0300

    criação do component react com o formulario de cadastro

--- resources/js/Pages/Cadastro/Index.jsx
+++ resources/js/Pages/Cadastro/Index.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { Head } from '@inertiajs/react';
+
 export default function Index(){
-    return 'Ola Mundo! Eu quero dançarrrrrrr';
+    return (
+        <div>
+                <Head title="Cadastros" />
+                
+                <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                    <form>
+                        <p>Nome: <input type="text" /></p>  
+                        <p>Preço: <input type="text" /></p>
+                        <PrimaryButton>Produto</PrimaryButton>
+                    </form>
+                </div>
+        </div>         
+    );
 }
