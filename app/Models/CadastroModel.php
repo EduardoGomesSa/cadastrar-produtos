c01a47893a1ecd25cc7c13d2e30d5b3cb7ceef92
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-21 23:53:02 -0300

    criação da relação entre usuario e cadastro

--- app/Models/CadastroModel.php
+++ app/Models/CadastroModel.php
@@ -17,4 +17,9 @@ class CadastroModel extends Model
 
     protected $table = 'produtos';
     protected $primaryKey = 'produto_id';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function cadastros()
+    {
+        return $this->hasMany(CadastroModel::class);
+    }
 }
