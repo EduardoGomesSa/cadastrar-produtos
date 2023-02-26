4c7377adca3a98e3249139884ca0c61a8a4bc2cd
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-25 22:16:40 -0300

    criação da estrutura do código html no blade

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -1,1 +1,12 @@
-<h1>Ola, aqui será o cadastro!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastro de Produtos</title>
+</head>
+<body>
+    <h1>Cadastro de Produtos - Blade</h1>
+</body>
+</html>
