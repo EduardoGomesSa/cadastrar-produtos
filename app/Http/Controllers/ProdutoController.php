86499d43c334fd373f6202b4e4830f4f762f4688
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-12 00:04:39 -0300

    criação dos arquivos de request e resource de produto e criação do método de buscar todos os produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ProdutoRequest;
+use App\Http\Resources\ProdutoResource;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    //
+    private $produto;
+
+    public function __construct(Produto $produto)
+    {
+        $this->produto = $produto;
+    }
+
+    public function index(){
+        return ProdutoResource::collection(
+            $this->produto->all()
+        );
+    }
 }
