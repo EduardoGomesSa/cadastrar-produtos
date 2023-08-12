7726c9c5b39c87d4a21ce69ba641cce28be1a965
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-11 23:37:30 -0300

    remoção da antiga migration de produto

--- database/migrations/2023_08_12_023133_create_produtos_table.php
+++ database/migrations/2023_08_12_023133_create_produtos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->decimal('preco');
             $table->timestamps();
         });
     }
