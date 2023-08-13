ec67015471f0a2b42c033c71b70eb5353b60f0ed
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-13 01:15:58 -0300

    definição das rotas de estoque

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EstoqueController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,10 @@ Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
 Route::put('/produtos/{id}', [ProdutoController::class, 'update']);
 Route::delete('/produtos/{id}', [ProdutoController::class, 'destroy']);
+
+Route::get('/estoques', [EstoqueController::class, 'index']);
+Route::post('/estoques', [EstoqueController::class, 'store']);
+Route::put('/estoques/{id}', [EstoqueController::class, 'update']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
